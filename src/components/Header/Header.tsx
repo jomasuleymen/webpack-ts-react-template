aa8ab09735c33ca94d2d46dc4a497896bc8300f4
Zhomart <zhomart200275@gmail.com>
2023-01-24 15:26:09 +0600

    removed unnecessary elements

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,28 @@
-import MainHeader from "./MainHeader";
+import { Layout } from "antd";
+import { NavLink } from "react-router-dom";
 
 import "./header.style.scss";
 
+const navigations = [
+	{ name: "Плейлист", href: "/playlist", key: "playlist" },
+	{ name: "Админ панель", href: "/admin", key: "admin" },
+];
+
 const Header: React.FC = () => {
 	return (
-		<header className="header">
-			<MainHeader />
-		</header>
+		<Layout.Header className="header">
+			<div className="width-wrapper" style={{ display: "flex", alignItems: "center" }}>
+				{navigations.map((item) => (
+					<NavLink
+						to={{ pathname: item.href }}
+						key={item.key}
+						className={({ isActive }) => (isActive ? "selected" : undefined)}
+					>
+						{item.name}
+					</NavLink>
+				))}
+			</div>
+		</Layout.Header>
 	);
 };
 
